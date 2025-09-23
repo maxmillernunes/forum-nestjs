f3aab9b2359b6f9ab70863db9d35f1cf24aa4ccc
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-09-23 15:47:52 -0300

    feat: :sparkles: add zod to validation pipe

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,27 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common'
+import { ZodError, ZodType } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodType) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      return this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: fromZodError(error),
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
