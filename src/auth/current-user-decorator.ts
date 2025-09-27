5426352aa446e1b9a41f6574fdd41203d443aaef
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-09-26 23:37:43 -0300

    feat: :sparkles: add route to get all questions

--- src/auth/current-user-decorator.ts
+++ src/auth/current-user-decorator.ts
@@ -1,9 +1,10 @@
 import { createParamDecorator, type ExecutionContext } from '@nestjs/common'
 import type { UserPayload } from './jwt.strategy'
+import type { Request } from 'express'
 
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest()
+    const request = context.switchToHttp().getRequest<Request>()
 
     return request.user as UserPayload
   }
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,5 @@
 import z from 'zod'
-import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
 import { CurrentUser } from 'src/auth/current-user-decorator'
