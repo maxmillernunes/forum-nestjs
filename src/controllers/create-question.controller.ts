b1a773940c42d2aa1285e9430c939003422318be
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-09-26 23:17:13 -0300

    feat: :sparkles: add route to create a question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -4,18 +4,48 @@ import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import type { UserPayload } from 'src/auth/jwt.strategy'
+import { PrismaService } from 'src/prisma/prisma.service'
 
-const createQuestionBodySchema = z.object({})
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @UseGuards(JwtAuthGuard)
 @Controller('/questions')
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    return 'ok'
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const userId = user.sub
+    const { content, title } = body
+
+    await this.prisma.question.create({
+      data: {
+        content,
+        title,
+        slug: this.toSlug(title),
+        authorId: userId,
+      },
+    })
+  }
+
+  private toSlug(title: string) {
+    return title
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9\s-]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-')
   }
 }
