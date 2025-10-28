272f2425dc147cd11845138792b29bb28df2c435
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-10-27 22:50:09 -0300

    feat: :sparkles: add new controller to delete answer

--- src/domain/forum/application/use-cases/delete-answer.ts
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,7 +1,8 @@
 import { left, right, type Either } from '@/core/either'
-import type { AnswersRepository } from '../repositories/answers-repository'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteAnswerRequest {
   answerId: string
@@ -13,6 +14,7 @@ type DeleteAnswerResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
---
+++ src/infra/http/controllers/delete-answer.controller.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { AppModule } from '@/infra/app.module'
+import request from 'supertest'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+import { AnswerFactory } from 'test/factories/make-answer'
+
+describe('Delete Questions (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+    jwt = moduleRef.get<JwtService>(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+
+    await app.init()
+  })
+
+  test('[DELETE] /answers/:id', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const token = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    const answerId = answer.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .delete(`/answers/${answerId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.status).toBe(204)
+
+    const answerOnDatabase = await prisma.answer.findUnique({
+      where: {
+        id: answerId,
+      },
+    })
+
+    expect(answerOnDatabase).toBeNull()
+  })
+})
