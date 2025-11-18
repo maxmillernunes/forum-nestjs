6abe2612f1c4b98b766a9ae5646fcf04f8ed3ca7
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-11-18 01:15:32 -0300

    fix: :sparkles: fix on cache and tests

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,18 +4,28 @@ import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { DomainEvents } from '@/core/events/domain-events'
 
+import { Redis } from 'ioredis'
+import { envSchema } from '@/infra/env/env'
+
 // Override env variables for test environment
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true }) // -> use when has any variable to override
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provide DATABASE_URL environment variable')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
@@ -24,12 +34,14 @@ function generateUniqueDatabaseURL(schemaId: string) {
 
 const schemaId = randomUUID()
 
-beforeAll(() => {
+beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
 
-  process.env.DATABASE_URL = databaseURL
+  env.DATABASE_URL = databaseURL
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('pnpm prisma migrate deploy')
 })
